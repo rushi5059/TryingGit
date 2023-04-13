68c622a05fa383f094b990ada69871adeaa6dbca
Rushikesh Pote <rushi5059@gmail.com>
2023-04-13 10:34:16 +0530

    change3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        // changing
+        // changing in main
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
